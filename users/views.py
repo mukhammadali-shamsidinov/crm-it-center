0a51fd37937b053da6ac1b81e0dba21776ceff8f
mukhammadali-shamsidinov <firebase1228@gmail.com>
2024-08-04 14:12:10 +0300

    cxr

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from users.models import CustomUser
+from users.serializers import CustomUserSerializer
+
+
+class ProfileAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = CustomUserSerializer(user, many=False)
+        return Response(serializer.data)
+
+
+
+
+
